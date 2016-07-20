9b88b5d0c0aa171318bd60ed7bc4e5caba8c3c34
Hsinhua Lin <hsinhualin@hsinhuademacbook-air.local>
2016-07-20 19:11:00 +0800

    Initial Commit

---
+++ 3rd Week/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3rd Week
+//
+//  Created by Hsinhua Lin on 2016/7/20.
+//  Copyright © 2016年 Hsinhua Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
